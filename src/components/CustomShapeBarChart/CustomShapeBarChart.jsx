655cf1f8131b4c12739178e1036f4f71ee2a6dec
isaiful508 <isaiful.islam508@gmail.com>
2024-03-27 11:11:29 +0600

    done chart section

--- src/components/CustomShapeBarChart/CustomShapeBarChart.jsx
+++ src/components/CustomShapeBarChart/CustomShapeBarChart.jsx
@@ -1,54 +1,10 @@
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import PropTypes from 'prop-types';
 
 
 
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
-
-// const data = [
-//     {
-//       name: 'Page A',
-//       uv: 4000,
-//       pv: 2400,
-//       amt: 2400,
-//     },
-//     {
-//       name: 'Page B',
-//       uv: 3000,
-//       pv: 1398,
-//       amt: 2210,
-//     },
-//     {
-//       name: 'Page C',
-//       uv: 2000,
-//       pv: 9800,
-//       amt: 2290,
-//     },
-//     {
-//       name: 'Page D',
-//       uv: 2780,
-//       pv: 3908,
-//       amt: 2000,
-//     },
-//     {
-//       name: 'Page E',
-//       uv: 1890,
-//       pv: 4800,
-//       amt: 2181,
-//     },
-//     {
-//       name: 'Page F',
-//       uv: 2390,
-//       pv: 3800,
-//       amt: 2500,
-//     },
-//     {
-//       name: 'Page G',
-//       uv: 3490,
-//       pv: 4300,
-//       amt: 2100,
-//     },
-//   ];
   
   const getPath = (x, y, width, height) => {
     return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
@@ -65,12 +21,12 @@ const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
 
 const CustomShapeBarChart = ({data}) => {
-    console.log(data);
-    
-     return (
-        <BarChart
-      width={650}
-      height={300}
+   
+      return (
+       <div className='flex justify-center items-center bg-[#13131308] rounded-xl mt-10'>
+         <BarChart
+      width={1170}
+      height={750}
       data={data}
       margin={{
         top: 20,
@@ -88,7 +44,20 @@ const CustomShapeBarChart = ({data}) => {
         ))}
       </Bar>
     </BarChart>
+       </div>
     );
 };
+
+
+
+
+CustomShapeBarChart.propTypes = {
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        bookName: PropTypes.string.isRequired,
+        totalPages: PropTypes.number.isRequired,
+      })
+    ).isRequired
+  };
 
 export default CustomShapeBarChart;
--- src/components/PagesToRead/PagesToRead.jsx
+++ src/components/PagesToRead/PagesToRead.jsx
@@ -14,8 +14,8 @@ const PagesToRead = () => {
     },[])
 
     return (
-        <div>
-            <h1>This is pages to read</h1>
+        <div >
+           
             <CustomShapeBarChart data={reads}></CustomShapeBarChart>
            
         </div>
